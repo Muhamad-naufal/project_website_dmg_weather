06e55a7a5e5c7af3fdcbb0a9be818db7b4e7a08d
muhamad-naufal <muhamadnaufal594@gmail.com>
2024-11-16 09:25:25 +0700

    update header

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,25 @@ import Layout from "./components/layout";
 import { ThemeProvider } from "./components/theme-provider";
 import WeatherDashboard from "../pages/weather-dashboard";
 import CityDashboard from "../pages/city-dashboard";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <BrowserRouter>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<WeatherDashboard />} />
-            <Route path="/city/" element={<CityDashboard />} />
-          </Routes>
-        </Layout>
-      </BrowserRouter>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <BrowserRouter>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<WeatherDashboard />} />
+              <Route path="/city/" element={<CityDashboard />} />
+            </Routes>
+          </Layout>
+        </BrowserRouter>
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,19 @@
+import { Link } from "react-router-dom";
 import { ModeToggle } from "./mode-toggle";
 
 const Header = () => {
   return (
-    <div>
-      <ModeToggle />
-    </div>
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur py-2 supports-[backdrops-filter]:bg-background/60">
+      <div className="container mx-auto flex h-16 items-center justify-between px-4">
+        <Link to="/">
+          <img src="/logo_tulisan.png" alt="logo" className="h-14" />
+        </Link>
+        <div>
+          {/* Search */}
+          <ModeToggle />
+        </div>
+      </div>
+    </header>
   );
 };
 
